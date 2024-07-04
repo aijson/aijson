d8f5b52ef21b5925824b2961171ef00185a96f87
Rafael Irgolic <hello@irgolic.com>
2024-07-05 00:33:56 +0200

    gradio: show outputs in tabs, use markdown

--- asyncflows/scripts/launch_gradio_preview.py
+++ asyncflows/scripts/launch_gradio_preview.py
@@ -1,15 +1,18 @@
 import argparse
 import os
 
-
-import gradio as gr
-
 from asyncflows import AsyncFlows
 from asyncflows.log_config import get_logger
+from asyncflows.models.config.flow import Loop
+from asyncflows.utils.action_utils import get_actions_dict
 from asyncflows.utils.async_utils import merge_iterators
 from asyncflows.utils.static_utils import get_flow_variables
 
 os.environ["GRADIO_ANALYTICS_ENABLED"] = "False"
+
+
+import gradio as gr  # noqa: E402
+
 
 css = """
 footer {visibility: hidden}
@@ -25,42 +28,61 @@ def construct_gradio_app(log, variables: set[str], flow: AsyncFlows):
 
         submit_button = gr.Button("Submit")
 
-        action_output_textboxes = {
-            action_id: gr.Textbox(label=action_id, interactive=False)
-            for action_id in flow.action_config.flow
-        }
+        actions_dict = get_actions_dict()
+
+        action_output_components = {}
+        for action_id, action_invocation in flow.action_config.flow.items():
+            # TODO handle loop
+            if isinstance(action_invocation, Loop):
+                continue
+            action = actions_dict[action_invocation.action]
+            outputs_type = action._get_outputs_type(action_invocation)
+            with gr.Accordion(action_id):
+                with gr.Tabs():
+                    for output_name, output_field in outputs_type.model_fields.items():
+                        if output_field.deprecated:
+                            continue
+                        full_output_name = f"{action_id}.{output_name}"
+                        with gr.Tab(output_name):
+                            action_output_components[full_output_name] = gr.Markdown(
+                                show_label=False,
+                            )
 
         async def handle_submit(*args):
             # TODO handle non-string inputs and outputs
             # Clear the output fields
             yield {
                 action_output_textbox: ""
-                for action_output_textbox in action_output_textboxes.values()
+                for action_output_textbox in action_output_components.values()
             }
 
             # Set the variables
             kwargs = {variable_name: arg for variable_name, arg in zip(variables, args)}
             ready_flow = flow.set_vars(**kwargs)
 
+            objects_and_coros = []
+            for (
+                output_target,
+                action_output_textbox,
+            ) in action_output_components.items():
+                objects_and_coros.append(
+                    (
+                        action_output_textbox,
+                        ready_flow.stream(output_target),
+                    )
+                )
+
             # Stream the variables
             async for output_textbox, outputs in merge_iterators(
                 log,
-                *zip(
-                    *[
-                        (
-                            action_output_textbox,
-                            ready_flow.stream(f"{action_id}.result"),
-                        )  # TODO this is hardcoded
-                        for action_id, action_output_textbox in action_output_textboxes.items()
-                    ]
-                ),
+                *zip(*objects_and_coros),
             ):
                 yield {output_textbox: outputs}
 
         submit_button.click(
             handle_submit,
             inputs=list(variable_textboxes.values()),
-            outputs=list(action_output_textboxes.values()),
+            outputs=list(action_output_components.values()),
         )
 
     return preview
