387b1c628315ba48088f6d4b449574d62ed174ca
Rafael Irgolic <hello@irgolic.com>
2024-07-07 21:30:28 +0200

    gradio: reload on yaml change

--- asyncflows/scripts/launch_gradio_preview.py
+++ asyncflows/scripts/launch_gradio_preview.py
@@ -1,5 +1,15 @@
 import argparse
+import contextlib
+import importlib
 import os
+import time
+import traceback
+from contextlib import contextmanager
+from functools import partial
+from pathlib import Path
+from unittest import mock
+
+from gradio.utils import SourceFileReloader, _remove_no_reload_codeblocks
 
 from asyncflows import AsyncFlows
 from asyncflows.log_config import get_logger
@@ -101,16 +111,106 @@ def create_flow_gradio_app(flow_path: str):
     return construct_gradio_app(log, variables, flow)
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--flow",
-        help="Path to flow for populating link fields with",
-        required=True,
-    )
+def watchfn(watch_file_path: str, reloader: SourceFileReloader):
+    """Watch python files in a given module.
 
-    args = parser.parse_args()
-    flow_path = args.flow
+    get_changes is adapted from uvicorn's default file watcher.
+    """
 
-    demo = create_flow_gradio_app(flow_path)
-    demo.launch()
+    # The thread running watchfn will be the thread reloading
+    # the app. So we need to modify this thread_data attr here
+    # so that subsequent calls to reload don't launch the app
+    from gradio.cli.commands.reload import reload_thread
+
+    reload_thread.running_reload = True
+
+    def get_changes() -> Path | None:
+        nonlocal last_mtime
+
+        file = Path(watch_file_path)
+
+        try:
+            mtime = file.stat().st_mtime
+        except OSError:  # pragma: nocover
+            return None
+
+        if last_mtime is None:
+            last_mtime = mtime
+            return None
+        elif mtime > last_mtime:
+            return file
+        return None
+
+    last_mtime: float | None = None
+    # Need to import the module in this thread so that the
+    # module is available in the namespace of this thread
+    module = importlib.import_module(reloader.watch_module_name)
+    while reloader.should_watch():
+        changed = get_changes()
+        if changed:
+            # print(f"Changes detected in: {changed}")
+            try:
+                changed_demo_file = _remove_no_reload_codeblocks(
+                    str(reloader.demo_file)
+                )
+                exec(changed_demo_file, module.__dict__)
+            except Exception:
+                print(
+                    f"Reloading {reloader.watch_module_name} failed with the following exception: "
+                )
+                traceback.print_exc()
+                last_mtime = None
+                reloader.alert_change("error")
+                reloader.app.reload_error_message = traceback.format_exc()
+                continue
+            demo = getattr(module, reloader.demo_name)
+            reloader.swap_blocks(demo)
+            last_mtime = None
+        time.sleep(0.05)
+
+
+preview_module = "asyncflows.scripts.launch_gradio_preview"
+preview_module_path = __file__
+env_baks = {}
+
+
+@contextmanager
+def patch_gradio(watch_filepath: str):
+    env_overrides = {
+        "GRADIO_WATCH_MODULE_NAME": preview_module,
+        "GRADIO_WATCH_DEMO_NAME": "flow_preview",
+        "GRADIO_WATCH_DEMO_PATH": preview_module_path,
+        "GRADIO_WATCH_DIRS": "1",
+    }
+
+    for env_var, val in env_overrides.items():
+        env_baks[env_var] = os.environ.get(env_var)
+        os.environ[env_var] = val
+
+    with mock.patch("gradio.utils.watchfn", partial(watchfn, watch_filepath)):
+        yield
+
+    for env_var, val in env_baks.items():
+        if val is None:
+            del os.environ[env_var]
+        else:
+            os.environ["env_var"] = val
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--flow",
+    help="Path to flow for populating link fields with",
+    required=True,
+)
+
+args = parser.parse_args()
+flow_path = args.flow
+
+context = contextlib.nullcontext()
+if gr.NO_RELOAD:
+    context = patch_gradio(flow_path)
+
+with context:
+    flow_preview = create_flow_gradio_app(flow_path)
+    flow_preview.launch()
