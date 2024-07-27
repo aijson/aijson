4fee1b0e23e3b25e34ebb632c3cf4a64fb55634c
Rafael Irgolic <hello@irgolic.com>
2024-07-28 00:43:52 +0200

    model: add gpt-4o-mini

--- asyncflows/models/config/model.py
+++ asyncflows/models/config/model.py
@@ -27,6 +27,7 @@ ModelType = (
     Annotated[
         Literal[
             "gpt-4o",
+            "gpt-4o-mini",
             "gpt-4-1106-preview",
             "gpt-4",
             "gpt-4-turbo",
