5c330a7d54bf486864a5b18fc89b8e8c94fe3c0a
Uros Mrkobrada <uros.mrkobrada@gmail.com>
2024-11-08 10:53:45 +0100

    test_subflow: replace load_config_file with Flow.from_file

--- aijson/tests/test_subflow.py
+++ aijson/tests/test_subflow.py
@@ -4,28 +4,24 @@ from aijson_ml.actions.llm import Inputs, Prompt
 from aijson import Flow
 from aijson.tests.resources.testing_actions import AddOutputs
 from aijson.utils.action_utils import get_actions_dict
-from aijson.utils.loader_utils import load_config_file
 
 
 async def test_basic_subflow(assert_no_errors):
-    config = load_config_file("aijson/tests/resources/use_basic_subflow.ai.yaml")
-    flow = Flow(config)
-    expectedOutput = AddOutputs(result=3)
+    flow = Flow.from_file("aijson/tests/resources/use_basic_subflow.ai.yaml")
+    expected_output = AddOutputs(result=3)
     output = await flow.run()
-    assert output == expectedOutput
+    assert output == expected_output
 
 
 async def test_subflow_result(assert_no_errors):
-    config = load_config_file("aijson/tests/resources/use_subflow_result.ai.yaml")
-    flow = Flow(config)
-    expectedOutput = 3
+    flow = Flow.from_file("aijson/tests/resources/use_subflow_result.ai.yaml")
+    expected_output = 3
     output = await flow.run()
-    assert output == expectedOutput
+    assert output == expected_output
 
 
 async def test_prompt_subflow(assert_no_errors):
-    config = load_config_file("aijson/tests/resources/use_prompt_subflow.ai.yaml")
-    flow = Flow(config)
+    flow = Flow.from_file("aijson/tests/resources/use_prompt_subflow.ai.yaml")
 
     outputs = "Hello! How can I assist you today?"
 
@@ -38,18 +34,17 @@ async def test_prompt_subflow(assert_no_errors):
 
 
 async def test_calling_subflow_in_subflow(assert_no_errors):
-    config = load_config_file(
+    flow = Flow.from_file(
         "aijson/tests/resources/subflows/call_subflow_in_subflow.ai.yaml"
     )
-    flow = Flow(config)
-    expectedOutput = AddOutputs(result=3)
+    expected_output = AddOutputs(result=3)
     output = await flow.run()
-    assert output == expectedOutput
+    assert output == expected_output
 
 
 async def test_subflow_name(assert_no_errors):
-    config = load_config_file("aijson/tests/resources/subflows/subflow_result.ai.yaml")
-    assert config.name == "subflow_with_result"
+    flow = Flow.from_file("aijson/tests/resources/subflows/subflow_result.ai.yaml")
+    assert flow.action_config.name == "subflow_with_result"
 
 
 async def test_available_subflows(assert_no_errors):
